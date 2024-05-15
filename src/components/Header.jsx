7b8a28e3bf50a1bffd82f323de793adcb5d55f5a
Tsikairis Nikolaos <tsiakiris.ni@gmail.com>
2024-05-15 12:58:19 +0300

    responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,11 @@
-import logo from "../image/portfolio-black-removebg-preview.png";
 import { useState } from "react";
+import logo from "../image/portfolio-black-removebg-preview.png";
 
 export default function Header() {
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleMenu = () => {
-    setIsOpen((isOpen) => !isOpen);
+    setIsOpen((prevIsOpen) => !prevIsOpen);
   };
 
   return (
@@ -16,25 +16,17 @@ export default function Header() {
           <h2>Tsiakiris Nikolaos</h2>
         </div>
       </a>
-      <div className="hamburger" onClick={toggleMenu}>
+      <div className={`hamburger ${isOpen ? "active" : ""}`} onClick={toggleMenu}>
         <div className="line"></div>
         <div className="line"></div>
         <div className="line"></div>
       </div>
       <nav className={isOpen ? "open" : ""}>
         <ul>
-          <li>
-            <a href="#about">About</a>
-          </li>
-          <li>
-            <a href="#resume">Resume</a>
-          </li>
-          <li>
-            <a href="#projects">Projects</a>
-          </li>
-          <li>
-            <a href="#contact">Contact</a>
-          </li>
+          <li><a href="#about" onClick={toggleMenu}>About</a></li>
+          <li><a href="#resume" onClick={toggleMenu}>Resume</a></li>
+          <li><a href="#projects" onClick={toggleMenu}>Projects</a></li>
+          <li><a href="#contact" onClick={toggleMenu}>Contact</a></li>
         </ul>
       </nav>
     </header>
